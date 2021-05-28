5a66d81073e5ae07bd6293394d87cbcaa2e1d7fb
Mani <im.manikanta@gmail.com>
2021-05-29 01:47:48 +0800

    test: write test cases

--- src/components/UsersList/UsersList.test.js
+++ src/components/UsersList/UsersList.test.js
@@ -5,7 +5,29 @@ import UsersList from './UsersList';
 
 const renderUsersList = (props) => {
   const defaultProps = {
-    className: 'my-classname'
+    className: 'my-classname',
+    fetchUsersFromBackend: () => {},
+    loading: false,
+    hasData: true,
+    data: [
+      {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        address: {
+          street: 'Kulas Light',
+          suite: 'Apt. 556',
+          city: 'Gwenborough',
+          zipcode: '92998-3874',
+          geo: {
+            lat: '-37.3159',
+            lng: '81.1496'
+          }
+        },
+        phone: '1-770-736-8031 x56442'
+      }
+    ]
   };
 
   return <UsersList {...defaultProps} {...props} />;
